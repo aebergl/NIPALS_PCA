ff189ac8b060f802b516e6af2080e856f1f97414
aebergl <aebergl@gmail.com>
2022-04-20 17:33:44 -0400

    Update NIPALS_PCA_Predict.m

--- NIPALS_PCA_Predict.m
+++ NIPALS_PCA_Predict.m
@@ -1,4 +1,4 @@
-function [T,DModX] = NIPALS_PCA_Predict(X,PCAmodel,varargin)
+function [T] = NIPALS_PCA_Predict(X,PCAmodel,varargin)
 % NIPALS_PCA PCA calculation using NIPALS algorithm, handles missing values
 %
 % USAGE:
@@ -19,7 +19,60 @@ if ~isstruct(PCAmodel) || ~isfield(PCAmodel,'P') || ~isfield(PCAmodel,'x_mean') 
 end
 
 %Check Dimensions
-
-if size(PCAmodel.P,2) ~= size(X,2)
-    
+if size(PCAmodel.P,1) ~= size(X,2)
+    error('The number of variables must agree');
 end
+
+% Check for missing values
+MVX = isnan(X); %Logical matrix with true for NaNs
+if ~any(MVX,'all')
+    MVX = [];  %No missing values,  MVX=[];
+else
+    X(MVX) = 0;
+    MVX = ~MVX; % Create the "Hole" matrix with 0 where there are NaNs
+end
+% Remove mean
+if ~isempty(PCAmodel.x_mean)
+    X = bsxfun(@minus, X, PCAmodel.x_mean); %same as X = X - (ones(N,1) * x_mean);
+    if ~isempty(MVX)
+        X(~MVX) = 0; %replace MV's with zeros
+    end
+
+end
+
+% Scale data
+if ~isempty(PCAmodel.x_weight)
+    X = bsxfun(@times, X, PCAmodel.x_weight); %same as X = X .* (ones(N,1) * x_weight);
+    if ~isempty(MVX)
+        X(~MVX) = 0; %replace MV's with zeros
+    end
+
+end
+[N,K] = size(X);
+T = zeros(N,PCAmodel.NumComp);
+
+
+for i=1:PCAmodel.NumComp
+    if isempty(MVX)
+        t = X * PCAmodel.P(:,i);
+    else
+        t = X * PCAmodel.W(:,i) ./ (MVX * PC.Model.W(:,i).^2);
+    end
+    T(:,i) = t;
+    X = X - t * PCAmodel.P(:,i)';
+    if ~isempty(MVX)
+        X(MVX) = 0;
+    end
+end
+
+
+% %Calculate DModX
+% if isempty(MV)
+%     [S1]= sqrt(((sum((X.*X)'))) / (K-NumComp));
+% else
+%     [S1]= sqrt(((sum((X.*X)'))) ./ (MV.mv_row-NumComp)');
+% end
+% RES.DmodX =  S1/PCAmodel.Model.S0(end);
+% RES.fcrit = PCAmodel.Model.fcrit(end);
+
+
